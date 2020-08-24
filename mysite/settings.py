89fa97d1c464f7c415f2f528157e2e363fe7fa29
Aidan <adssoccer1@gmail.com>
2020-08-24 16:31:04 -0400

    added problem type functionality

--- mysite/settings.py
+++ mysite/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '0==50(rjb7%n@(*f)2ut+7sam+^%#f$mik%ck@9ici)xus#@*5'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
