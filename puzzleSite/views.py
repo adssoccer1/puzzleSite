ff921a0af430c519e52283df9a1be364b5706803
Aidan <adssoccer1@gmail.com>
2020-08-24 16:08:37 -0400

    added problem type functionality

--- puzzleSite/views.py
+++ puzzleSite/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+from .models import Post
 
-# Create your views here.
+
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'post_detail.html', {'post': post})
+
+
+def post_type(request, type):
+    print("here", type)
+    posts = Post.objects.filter(tags__contains=type).order_by('published_date')
+    print(posts)
+    return render(request, 'post_type.html', {'posts':posts})
